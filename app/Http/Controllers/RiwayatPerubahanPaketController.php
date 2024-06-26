3613f30701faad69e712bbd7053c291956a43766
rondi91 <rondie91@gmail.com>
2024-06-27 06:45:41 +0700

    php artisan make:model RiwayatPerubahanPaket -a

---
+++ app/Http/Controllers/RiwayatPerubahanPaketController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RiwayatPerubahanPaket;
+use App\Http\Requests\StoreRiwayatPerubahanPaketRequest;
+use App\Http\Requests\UpdateRiwayatPerubahanPaketRequest;
+
+class RiwayatPerubahanPaketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRiwayatPerubahanPaketRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RiwayatPerubahanPaket $riwayatPerubahanPaket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RiwayatPerubahanPaket $riwayatPerubahanPaket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRiwayatPerubahanPaketRequest $request, RiwayatPerubahanPaket $riwayatPerubahanPaket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RiwayatPerubahanPaket $riwayatPerubahanPaket)
+    {
+        //
+    }
+}
