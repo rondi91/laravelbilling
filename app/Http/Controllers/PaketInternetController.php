35656d9e56f7b504f8ba1f98c9b038e42d8ae704
rondi91 <rondie91@gmail.com>
2024-06-26 17:14:03 +0700

    artisan make:model paketInternet -a

---
+++ app/Http/Controllers/PaketInternetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaketInternet;
+use App\Http\Requests\StorePaketInternetRequest;
+use App\Http\Requests\UpdatePaketInternetRequest;
+
+class PaketInternetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaketInternetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaketInternet $paketInternet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaketInternet $paketInternet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaketInternetRequest $request, PaketInternet $paketInternet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaketInternet $paketInternet)
+    {
+        //
+    }
+}
